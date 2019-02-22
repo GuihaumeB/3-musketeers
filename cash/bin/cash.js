b1681b474b3ac8c8ec52681a0cf2b90dff6df8bf
GuihaumeB <guihaume.buevoz@devinci.fr>
2019-02-22 14:48:21 +0100

    Comments added

--- cash/bin/cash.js
+++ cash/bin/cash.js
@@ -5,15 +5,19 @@ const got = require('got');
 const money = require('money');
 const chalk = require('chalk');
 const ora = require('ora');
+//This json file contains detailed information on currencies
 const currencies = require('../lib/currencies.json');
 
+//Get required API to fetch currency exchange rates
 const {API} = require('./constants');
 
+//Ansyncronous command in order to avoid overlapping queries
 const cash = async command => {
 	const {amount} = command;
 	const from = command.from.toUpperCase();
 	const to = command.to.filter(item => item !== from).map(item => item.toUpperCase());
 
+	//Displays text for the user
 	console.log();
 	const loading = ora({
 		text: 'Converting...',
@@ -33,9 +37,9 @@ const cash = async command => {
 		money.rates = response.body.rates;
 
 		to.forEach(item => {
-			if (currencies[item]) {
+			if (currencies[item]) {//Checks if the input currency exists in the file
 				loading.succeed(`${chalk.green(money.convert(amount, {from, to: item}).toFixed(3))} ${`(${item})`} ${currencies[item]}`);
-			} else {
+			} else {//Display error message
 				loading.warn(`${chalk.yellow(`The "${item}" currency not found `)}`);
 			}
 		});
@@ -51,4 +55,5 @@ const cash = async command => {
 	});
 };
 
+//Displays result
 module.exports = cash;
